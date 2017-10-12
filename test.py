66d62b4891ce952a02ee5e77e4b9dacbe47fb81b
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-10-12 17:21:36 +0800

    Clean code file

--- test.py
+++
@@ -1,9 +0,0 @@
-try:
-    from .auto import FDC
-except Exception as e:
-    from auto import FDC
-
-
-if __name__ == '__main__':
-    fdc = FDC(glass_id='TL6AJ0HAV')
-    print(fdc.get_array_pds())
