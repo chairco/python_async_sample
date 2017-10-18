d9b7d992f310229fc1b7b58bc3813e701e9d0a3c
yc <yi-chieh.yc.chen@mail.foxconn.com>
2017-10-18 10:15:22 +0800

    Add query teg api

--- query.py
+++ query.py
@@ -19,12 +19,11 @@ BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output')
 MAX_WORKER = 200
 
 
-class Queryedc:
+class Querybase:
     """
-    Here is API for query oracle db with high level multiprocess. 
-    :types query: auto.query object
+    Here is query base with concurrency.
     """
-    def _query_histroy_concurrency(self, query, glass_id):
+    def _query_history_concurrency(self, query, glass_id):
         """
         Query oracle db by mutiplethread
         :type query: query object
@@ -73,29 +72,59 @@ class Queryedc:
                 result.setdefault(g_s_id, data)
         return result
 
+    def _query_rawdata_concurrency(self):
+        pass
+
+
+class Queryedc(Querybase):
+    """
+    Here is API for query oracle db with high level multiprocess. 
+    :types query: auto.query object
+    """
     @checktypes
     def glass_history(self, glass_id: list):
-        return self._query_histroy_concurrency(auto.get_edc_glass_history, glass_id)
+        return self._query_history_concurrency(auto.get_edc_glass_history, glass_id)
 
     @checktypes
     def glass_data(self, glass_id: list):
-        sid_dataset = self._query_histroy_concurrency(
-            auto.get_edc_glass_history, glass_id)
+        sid_dataset = self.glass_history(glass_id)
         values = list(chain.from_iterable(sid_dataset.values()))
         return self._query_data_concurrency(auto.get_edc_data, values)
 
 
-class Queryteg:
+class Queryteg(Querybase):
     """
     Here is API for query oracle db with high level multprocess.
     """
-    def _query_history_concurrency(self):
-        pass
+    @checktypes
+    def glass_history(self, glass_id: list):
+        return self._query_history_concurrency(auto.get_teg_glass_history, glass_id)
 
-    def _query_rawdata_concurrency(self):
-        pass
+    @checktypes
+    def glass_data(self, glass_id: list):
+        sid_dataset = self.glass_history(glass_id)
+        values = list(chain.from_iterable(sid_dataset.values()))
+        return self._query_data_concurrency(auto.get_teg_data, values)
 
-    def _query_summary_concurrency(self):
-        pass
 
+if __name__ == '__main__':
+    with open('sample.csv', 'r') as fp:
+        glass_id = fp.readlines()
+    glass_id = [i.rstrip() for i in glass_id]
+    
+    # test teg
+    t0 = time.time()
+    q = Queryteg()
+    ret = q.glass_data(glass_id)
+    elapsed = time.time() - t0
+    msg = '\n{} glass_id query in {:.2f}s'
+    print(msg.format(len(ret), elapsed))
 
+    
+    # test edc
+    t0 = time.time()
+    q = Queryedc()
+    ret = q.glass_history(glass_id)
+    elapsed = time.time() - t0
+    msg = '\n{} glass_id query in {:.2f}s'
+    print(msg.format(len(ret), elapsed))
