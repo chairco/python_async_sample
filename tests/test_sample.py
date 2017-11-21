73f7c8b097a5071147580cb59767f7ec11d06a7d
chairco <chairco@gmail.com>
2017-11-21 15:20:54 +0800

    Update testcase

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,9 +1,9 @@
 # content of test_sample.py
 import unittest
 import datetime
+import pytest
 
 from nikon_ETL import get_lastendtime, ckflow
-
 
 
 def func(x):
@@ -17,16 +17,32 @@ def test_answer():
 class TestFormat(unittest.TestCase):
 
     def setUp(self):
-        self.row = [{'apname': 'EDC_Import', 'last_end_time': datetime.datetime(2017, 10, 26, 23, 31, 27), 'virtual_recipe': None}]
+        self.row = [{'apname': 'EDC_Import', 'last_end_time': datetime.datetime(
+            2017, 10, 26, 23, 31, 27), 'virtual_recipe': None}]
         self.endtime_data = []
 
     def test_get_lastendtime(self):
-        #row = [{'apname': 'EDC_Import', 'last_end_time': datetime.datetime(2017, 10, 26, 23, 31, 27), 'virtual_recipe': None}]
         lastendtime = get_lastendtime(row=self.row)
-        assert lastendtime == datetime.datetime.strptime('2017-10-26 23:31:27', '%Y-%m-%d %H:%M:%S')
+        assert lastendtime == datetime.datetime.strptime(
+            '2017-10-26 23:31:27', '%Y-%m-%d %H:%M:%S')
 
     def test_ckflow(self):
-        #row = [{'apname': 'EDC_Import', 'last_end_time': datetime.datetime(2017, 10, 26, 23, 31, 27), 'virtual_recipe': None}]
         assert ckflow(row=self.row) == True
 
+    def test_column_state(self):
+        pass
 
+    def test_clean_edcdata(self):
+        pass
+
+    def test_clean_schemacolnames(self):
+        pass
+
+
+class TestDB(unittest.TestCase)
+    """docstring for TestDB
+    should init a mock db
+    """
+    
+    def setUp(self):
+        pass
